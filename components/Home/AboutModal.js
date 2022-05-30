e36fa7bf1575dbb5d9237dff8b16714aa060c8fb
Omer Faran <haveri321@gmail.com>
2022-05-30 17:11:12 +0300

    app youtube vid updated link

--- components/Home/AboutModal.js
+++ components/Home/AboutModal.js
@@ -25,7 +25,7 @@ const AboutModal = ({ modalClose, showModal }) => {
               <iframe
                 width="100%"
                 height="315"
-                src="https://www.youtube.com/embed/439pP_kn76w"
+                src="https://www.youtube.com/embed/XIBOSzDBvZs"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
